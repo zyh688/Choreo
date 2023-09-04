40805994e6cc4fd31b613a9ebb28e191fc7b42fc
zyh688 <66469155+zyh688@users.noreply.github.com>
2023-09-04 16:41:02 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,8 +3,6 @@
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-WEB_USERNAME=${WEB_USERNAME:-'admin'}
-WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
 generate_config() {
   cat > /tmp/config.json << EOF
