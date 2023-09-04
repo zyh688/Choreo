54031a39590b01ca508ad8b13a6fee655e3da8ab
zyh688 <66469155+zyh688@users.noreply.github.com>
2023-09-04 16:35:51 +0800

    Update server.js

--- files/server.js
+++ files/server.js
@@ -1,5 +1,3 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
 const url = `https://${process.env.WEB_DOMAIN}`;
 const port = process.env.PORT || 3000;
 const express = require("express");
@@ -14,16 +12,6 @@ const auth = require("basic-auth");
 
 app.get("/", function (req, res) {
   res.send("hello world");
-});
-
-// 页面访问密码
-app.use((req, res, next) => {
-  const user = auth(req);
-  if (user && user.name === username && user.pass === password) {
-    return next();
-  }
-  res.set("WWW-Authenticate", 'Basic realm="Node"');
-  return res.status(401).send();
 });
 
 //获取系统进程表
